00923789c3098f9d5db16498351f684611049661
mantle0x <127942985+mantle0x@users.noreply.github.com>
2023-12-26 16:24:48 +0100

    Create gov.sol

---
+++ gov.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title Mantle0x Governance and DAO Contract
+ * This contract provides basic governance and DAO functionalities for Mantle0x, with omnichain support.
+ */
+
+contract Mantle0x Governance {
+    address public owner;
+    uint public minimumQuorum;
+    uint public debatingPeriodDuration;
+    uint public proposalCount;
+
+    struct Proposal {
+        uint id;
+        string description;
+        bytes callData;
+        address recipient;
+        uint createdAt;
+        uint votesFor;
+        uint votesAgainst;
+        bool executed;
+        mapping(address => bool) voted;
+    }
+
+    mapping(uint => Proposal) public proposals;
+    mapping(address => uint) public tokenBalances;
+
+    event ProposalCreated(uint id, string description, address recipient);
+    event VoteCasted(uint proposalId, address voter, bool vote, uint votes);
+    event ProposalExecuted(uint proposalId, bool result);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function.");
+        _;
+    }
+
+    constructor(uint _minimumQuorum, uint _debatingPeriodDuration) {
+        owner = msg.sender;
+        minimumQuorum = _minimumQuorum;
+        debatingPeriodDuration = _debatingPeriodDuration;
+    }
+
+    function createProposal(string memory description, bytes memory callData, address recipient) public onlyOwner {
+        uint proposalId = proposalCount++;
+        Proposal storage p = proposals[proposalId];
+        p.id = proposalId;
+        p.description = description;
+        p.callData = callData;
+        p.recipient = recipient;
+        p.createdAt = block.timestamp;
+        emit ProposalCreated(proposalId, description, recipient);
+    }
+
+    function vote(uint proposalId, bool support) public {
+        Proposal storage p = proposals[proposalId];
+        require(block.timestamp < p.createdAt + debatingPeriodDuration, "Debating period is over");
+        require(!p.voted[msg.sender], "Already voted");
+        require(tokenBalances[msg.sender] > 0, "No tokens to vote");
+
+        p.voted[msg.sender] = true;
+        if (support) {
+            p.votesFor += tokenBalances[msg.sender];
+        } else {
+            p.votesAgainst += tokenBalances[msg.sender];
+        }
+
+        emit VoteCasted(proposalId, msg.sender, support, tokenBalances[msg.sender]);
+    }
+
+    function executeProposal(uint proposalId) public {
+        Proposal storage p = proposals[proposalId];
+        require(block.timestamp >= p.createdAt + debatingPeriodDuration, "Debating period not over");
+        require(!p.executed, "Proposal already executed");
+        require(p.votesFor > p.votesAgainst && p.votesFor + p.votesAgainst >= minimumQuorum, "Proposal did not pass");
+
+        p.executed = true;
+        (bool success, ) = p.recipient.call(p.callData);
+        emit ProposalExecuted(proposalId, success);
+    }
+
+    function depositTokens(uint amount) public {
+        // Logic for token deposit
+    }
+
+    function withdrawTokens(uint amount) public {
+        // Logic for token withdrawal
+    }
+
+    // Additional functions and logic...
+}
